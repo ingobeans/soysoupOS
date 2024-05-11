2103756fa8c65bfd00a6685db7440107b7145b15
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-05-12 00:19:40 +0200

    Replaced the program bundler with a filesystem bundler. Will recreate an folder structure to a string loadable by the SoyFileSystem

---
+++ filesystem_bundler.py
@@ -0,0 +1,28 @@
+import os, json
+
+def create_file_dict(file_path):
+    with open(file_path, 'r') as f:
+        content = f.read()
+    return {"type": "file", "content": content}
+
+def create_directory_dict(directory_path):
+    directory_dict = {"type": "directory", "content": {}}
+    for entry in os.listdir(directory_path):
+        entry_path = os.path.join(directory_path, entry)
+        if os.path.isfile(entry_path):
+            if entry.endswith(".js"):
+                entry = entry.removesuffix(".js") + ".soup"
+            # convert .js to executable .soup files instead
+
+            directory_dict["content"][entry] = create_file_dict(entry_path)
+        elif os.path.isdir(entry_path):
+            directory_dict["content"][entry] = create_directory_dict(entry_path)
+    return directory_dict
+
+folder_path = "soy_filesystem"
+soy_file_system_dict = create_directory_dict(folder_path)
+
+result = repr(json.dumps(soy_file_system_dict))
+
+with open("programs.js","w",encoding="utf-8") as f:
+    f.write(f"systemFiles = {result}")
