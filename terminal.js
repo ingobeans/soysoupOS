70f4100b2bda3891b501f66bb0a1c8bf5c8144da
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-05-12 19:56:54 +0200

    Fixed F12 from being blocked on page

--- terminal.js
+++ terminal.js
@@ -47,7 +47,10 @@ document.addEventListener("keydown", function (event) {
   if (event.ctrlKey && (event.key == "c" || event.key == "v")) {
     return;
   }
-  if (event.key.length == 2 && event.key.startsWith("F")) {
+  if (
+    (event.key.length == 2 || event.key.length == 3) &&
+    event.key.startsWith("F")
+  ) {
     return;
   }
   event.preventDefault();
