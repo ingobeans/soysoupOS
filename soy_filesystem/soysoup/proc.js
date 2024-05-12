e755e3df7fd4bf33e5101f15e4b4fc1cc4465ba3
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-05-12 11:51:43 +0200

    Added proc kill option

--- soy_filesystem/soysoup/proc.js
+++ soy_filesystem/soysoup/proc.js
@@ -1,10 +1,15 @@
 class ProgramSource extends Program {
   load(args) {
     var parts = parseToParts(args);
-    if (!args || (parts[0] != "list" && parts[0] != "kill")) {
+    if (
+      !args ||
+      (parts[0] != "list" && parts[0] != "kill") ||
+      (parts[0] == "kill" && parts.length == 1)
+    ) {
       this.outputShell.println(
         `error: missing or incorrect args. use 'proc list' to list processes or 'proc kill <process ID>' to terminate a process.`
       );
+      // invalid args if args is empty, if the first argument is not list or kill, or if the first argument is kill but there is no argument for process ID
       this.quit();
       return;
     }
@@ -19,6 +24,9 @@ class ProgramSource extends Program {
           program.filepath.split("/")[program.filepath.split("/").length - 1];
       }
       this.outputShell.println(text);
+    } else if (parts[0] == "kill") {
+      programs.splice(Number(parts[1]), 1);
+      this.outputShell.println("killed process with ID " + parts[1]);
     }
     this.quit();
   }
