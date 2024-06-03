b92a51dfcc92118b93f85fd7706231496e555f32
Klaar Ingemar <127751491+ingobeans@users.noreply.github.com>
2024-06-03 10:10:38 +0200

    Improved error handling

--- system.js
+++ system.js
@@ -1,4 +1,4 @@
-systemVersion = "0.3.0";
+systemVersion = "0.3.2";
 
 fileSystem = new SoyFileSystem();
 
@@ -9,11 +9,13 @@ function printConsole(string) {
   printOut(string);
 }
 
-function handleProgramError(msg){
+function handleProgramError(e){
+  var msg = e.toString()
   switch(msg){
     case "ReferenceError: WindowComponent is not defined":
-      return "Program was designed to run in a desktop environment"
-      break;
+      return "program was designed to run in a desktop environment";
+    case "ReferenceError: ProgramSource is not defined":
+      return "program has no entry point";
     default:
       return msg;
   }
@@ -187,17 +189,12 @@ function executeFile(path, argsRaw, outputShell, cwd) {
     exitResolve = resolve;
   });
   var data = fileSystem.readFile(path);
-  if (data.includes("ProgramSource") == false) {
-    outputShell.println(error("the program " + path + " has no entry point."));
-    return;
-  }
   try {
     eval(data + "\nprograms.unshift(new ProgramSource)");
   } catch (e) {
     if (e) {
-      console.log(e.toString());
       outputShell.println(
-        error("the program " + path + " couldn't run.\n" + handleProgramError(e.toString())),
+        error("the program " + path + " couldn't run.\n" + handleProgramError(e)),
       );
       return;
     }
