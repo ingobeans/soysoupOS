a48d9167490da4776f17d673c17cfa55f444043b
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-08-23 23:43:19 +0200

    better help message

--- soy_filesystem/soysoup/help.js
+++ soy_filesystem/soysoup/help.js
@@ -1,19 +1,21 @@
 class ProgramSource extends Program {
   load(args) {
-    var helpMessage = `cd - navigate to another directory
-cat - read contents of file
-echo - print a string to shell
-edit - edit file by path
-exit - close terminal instance
-help - prints this
-ls - list contents of directory
-mkdir - create folder
-proc - list or terminate processes
-pwd - prints working directory
-reload - reset file system
-rm - remove file or folder
-cls - clears the terminal
-terminal - open new instance of terminal`;
+    var helpMessage = `cd [directory]       - navigate to another directory
+cat [file]           - read contents of file
+echo [string]        - print a string to shell
+edit [file]          - edit file by path
+exit                 - close terminal instance
+help                 - prints this
+ls <directory>       - list contents of directory
+mkdir [directory]    - create folder
+proc list            - list processes
+proc kill [pid]      - terminate process
+pwd                  - prints working directory
+reload               - reset file system
+rm [file/directory]  - remove file or folder
+mv [source file] [dest file]   - move or rename a file from source to destination
+cls                  - clears the terminal
+terminal             - open new instance of terminal`;
     this.outputShell.println(helpMessage);
     this.quit();
   }
