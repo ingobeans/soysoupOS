5fece9118d69412ad072dcc255a96a17a9a1ae3c
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-08-27 21:50:13 +0200

    add service list all/running

--- soy_filesystem/soysoup/bin/service.js
+++ soy_filesystem/soysoup/bin/service.js
@@ -4,28 +4,41 @@ class ProgramSource extends Program {
     if (
       !args ||
       (parts[0] != "list" && parts[0] != "start" && parts[0] != "stop") ||
-      (parts[0] == "start" && parts.length == 1) ||
-      (parts[0] == "stop" && parts.length == 1)
+      (parts[0] == "list" && parts[1] != "running" && parts[1] != "all") ||
+      parts.length != 2
     ) {
       this.outputShell.println(
         error(
-          `missing or incorrect args. use 'services list' to list running services or 'service start/stop <service name>' to start/stop a service`
+          `missing or incorrect args. use 'services list running/all' to list services or 'service start/stop <service name>' to start/stop a service`
         )
       );
       this.quit();
       return;
     }
     if (parts[0] == "list") {
+      let runningServices = Object.keys(serviceManager.serviceInstances);
       let servicesText = "";
-      for (let service of Object.keys(serviceManager.serviceInstances)) {
-        let instance = serviceManager.serviceInstances[service];
-        servicesText +=
-          "\n\t" +
-          instance.filepath
-            .split("/")
-            [instance.filepath.split("/").length - 1].split(".")[0];
+      if (parts[1] == "running") {
+        servicesText = "running services:";
+        for (let service of runningServices) {
+          let instance = serviceManager.serviceInstances[service];
+          servicesText +=
+            "\n\t" +
+            instance.filepath
+              .split("/")
+              [instance.filepath.split("/").length - 1].split(".")[0];
+        }
+      } else {
+        servicesText = "all services:";
+        for (let service of serviceManager.services) {
+          let color = "";
+          if (!runningServices.includes(service)) {
+            color = MUTED_COLOR;
+          }
+          servicesText += "\n\t" + color + service + RESET_COLOR;
+        }
       }
-      this.outputShell.println("services running: " + servicesText);
+      this.outputShell.println(servicesText);
       this.quit();
     } else if (parts[0] == "start") {
       serviceManager.startService(parts[1], this.outputShell);
