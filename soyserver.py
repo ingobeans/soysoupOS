c10f6fbc43cdde841eb8d84f9aa156ed0b53c428
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-12-21 20:57:31 +0100

    an optional custom httpserver that automatically rebuilds the filesystem as nescessary

---
+++ soyserver.py
@@ -0,0 +1,23 @@
+import http.server, socketserver, filesystem_bundler
+
+PORT = 33003
+class RequestHandler(http.server.SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/programs.js":
+            filesystem_bundler.update_programs()
+            print("rebuilt filesystem!")
+        return super().do_GET()
+    def end_headers(self):
+        self.send_header('Access-Control-Allow-Origin', '*')
+        super().end_headers()
+
+handler_object = RequestHandler
+with socketserver.TCPServer(("", PORT), handler_object) as httpd:
+    print(f"serving at: http://localhost:{PORT}")
+    print("press ctrl+c to stop")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print("stopped soyserver")
